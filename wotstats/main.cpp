7088e3cde3d176087d55cecbed68771195bca76c
Jan Temmerman <jantemmerman@mac-jan.local>
2012-05-14 17:17:22 +0200

    Initial Commit

---
+++ wotstats/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  wotstats
+//
+//  Created by Jan Temmerman on 14/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
