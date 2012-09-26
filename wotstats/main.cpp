ad8bf44b5c4c76a094bb1f29c0c1396f7c4bf47a
Jan Temmerman <jantemmerman@mac-jan.local>
2012-09-26 22:47:44 +0200

    - generate death map - modularize code

--- wotstats/main.cpp
+++ wotstats/main.cpp
@@ -38,14 +38,14 @@ using namespace wotstats;
 using namespace tbb;
 using namespace boost::filesystem;
 using namespace boost::accumulators;
-
+using namespace boost;
 using boost::accumulators::left;
 using boost::accumulators::right;
 // tolerance in %
 double epsilon = 1;
 
 std::size_t n = 100000; // number of MC steps
-std::size_t c =  10000; // cache size
+std::size_t c =  250000; // cache size
 
 typedef accumulator_set<double, stats<tag::tail_quantile<boost::accumulators::right>>> accumulator_t_right;
 typedef accumulator_set<double, stats<tag::tail_quantile<boost::accumulators::left>>> accumulator_t_left;
@@ -218,7 +218,7 @@ void display_boundaries(const game_info &game_info, std::vector<packet_t> packet
     printf("%f %f %f %f\n", min_x, max_x, min_y, max_y);
 }
 
-bool write_png(const std::string &out, png_bytepp image, int width, int height, bool alpha) {
+bool write_png(const std::string &out, png_bytepp image, size_t width, size_t height, bool alpha) {
     FILE *fp = fopen(out.c_str(), "wb");
 
     png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
@@ -307,7 +307,7 @@ struct process_result {
     bool error;
     std::string path;
     replay_file *replay;
-    array<image_t<float>, 2> position_image, death_image, hit_image;
+    std::array<image_t<float>, 2> position_image, death_image, hit_image;
     process_result(std::string path, int image_width, int image_height)
         : path(path), position_image({
             image_t<float>(boost::extents[image_width][image_height]),
@@ -352,10 +352,6 @@ void process_replay_directory(const path& directory) {
             auto path = it->path();
             file_path = path.string();
         }
-        if (count > 0) {
-            fc.stop();
-        }
-        count += 1;
         return new process_result{ file_path, 500, 500 };
     };
 
@@ -368,7 +364,7 @@ void process_replay_directory(const path& directory) {
             ifstream is(result->path, std::ios::binary);
             result->replay = new replay_file(is);
             is.close();
-        } catch (exception &e) {
+        } catch (std::exception &e) {
             result->error = true;
             std::cerr << "Error!" << std::endl;
             result->replay = nullptr;
@@ -376,7 +372,7 @@ void process_replay_directory(const path& directory) {
         return result;
     };
 
-    auto f_draw_position = [](const packet_t &packet, process_result &result) {
+    auto f_draw_position = [](const packet_t &packet, process_result &result, std::array<image_t<float>, 2> &images) {
         uint32_t player_id = packet.player_id();
         const replay_file &replay = *result.replay;
         const game_info &game_info = replay.get_game_info();
@@ -392,45 +388,54 @@ void process_replay_directory(const path& directory) {
         std::tie(x,y) = get_2d_coord(position, game_info, width, height);
         
         if (__builtin_expect(x < 0 || y < 0 || x >= (width - 1) || y >= (height - 1), 0)) {
-            if (!result.error) {
-#if DEBUG
-            std::cerr
-                << "WARNING: incorrect limits for " << game_info.map_name
-                << " x: " << x << " y: " << y
-                << " (" << std::get<0>(position) << "/" << std::get<2>(position) << ")"
-                << std::endl;
-#endif
-            result.error = true;
-            }
+//            if (!result.error) {
+//#if DEBUG
+//            std::cerr
+//                << "WARNING: incorrect limits for " << game_info.map_name
+//                << " x: " << x << " y: " << y
+//                << " (" << std::get<0>(position) << "/" << std::get<2>(position) << ")"
+//                << std::endl;
+//#endif
+//            result.error = true;
+//            }
         } else {
             float px = x - floor(x), py = y - floor(y);
-            result.position_image[team_id][floor(y)][floor(x)] += px*py;
-            result.position_image[team_id][ceil(y)][floor(x)] += px*(1-py);
-            result.position_image[team_id][floor(y)][ceil(x)] += (1-px)*py;
-            result.position_image[team_id][ceil(y)][ceil(x)] += (1-px)*(1-py);
+            images[team_id][floor(y)][floor(x)] += px*py;
+            images[team_id][ceil(y)][floor(x)] += px*(1-py);
+            images[team_id][floor(y)][ceil(x)] += (1-px)*py;
+            images[team_id][ceil(y)][ceil(x)] += (1-px)*(1-py);
         }
     };
     
     auto f_draw_death = [](const packet_t &packet, process_result &result) {
-
+        
     };
     
     auto f_process_replays = [&](process_result *result) -> process_result* {
         if (result->error) return result;
+
+        std::fill(result->position_image[0].data(), result->position_image[0].data() + 500*500, 0.f);
         const replay_file &replay = *result->replay;
         std::set<uint32_t> dead_players;
+
         for (const packet_t &packet : replay.get_packets()) {
-            if (packet.has_property(property::position)
-                && packet.has_property(property::player_id)
-                && dead_players.find(packet.player_id()) == dead_players.end()) {
-                f_draw_position(packet, *result);
-            }
             if (packet.has_property(property::tank_destroyed)) {
                 f_draw_death(packet, *result);
                 uint32_t killer, killed;
                 std::tie(killed, killer) = packet.tank_destroyed();
                 dead_players.insert(killed);
+                packet_t position_packet;
+                bool found = result->replay->find_property(packet.clock(), killed, property::position, position_packet);
+                if (found) {
+                    f_draw_position(position_packet, *result, result->death_image);
+                }
             }
+            if (packet.has_property(property::position)
+                && packet.has_property(property::player_id)
+                && dead_players.find(packet.player_id()) == dead_players.end()) {
+                f_draw_position(packet, *result, result->position_image);
+            }
+            
         }
         return result;
     };
@@ -464,7 +469,7 @@ void process_replay_directory(const path& directory) {
             }
         }
         std::stringstream out_file_name;
-        out_file_name << "out/" << result->path << ".png";
+        out_file_name << "out/positions/" << result->path << ".png";
         write_png(out_file_name.str(), row_pointers.get(), width, height, alpha);
         return result;
     };
@@ -478,9 +483,9 @@ void process_replay_directory(const path& directory) {
         };
     };
     
-    std::map<std::tuple<std::string, std::string>, std::array<image_t<float>, 2>> images;
+    std::map<std::tuple<std::string, std::string>, std::array<image_t<float>, 4>> images;
     auto f_merge_results = [&](process_result *result) -> process_result* {
-        if (result->error) return result;
+        if (result->error || result->replay->get_game_end().size() == 0 ) return result;
 
         const replay_file &replay = *result->replay;
         const game_info &game_info = replay.get_game_info();
@@ -488,18 +493,23 @@ void process_replay_directory(const path& directory) {
         auto key = std::make_tuple(game_info.map_name, game_info.game_mode);
         if (images.find(key) == images.end()) {
             images.insert(std::make_pair(key,
-                std::array<image_t<float>,2>({
+                std::array<image_t<float>,4>({
+                    image_t<float>(boost::extents[500][500]),
+                    image_t<float>(boost::extents[500][500]),
                     image_t<float>(boost::extents[500][500]),
                     image_t<float>(boost::extents[500][500])
                 })));
         }
 
-        std::array<image_t<float>,2> &image = images[key];
+        std::array<image_t<float>,4> &image = images[key];
 
         auto shape = image[0].shape();
         auto size = shape[0]*shape[1];
+        
         parallel_for(blocked_range<size_t>(0, size), merge_image(image[0], result->position_image[0]), auto_partitioner());
         parallel_for(blocked_range<size_t>(0, size), merge_image(image[1], result->position_image[1]), auto_partitioner());
+        parallel_for(blocked_range<size_t>(0, size), merge_image(image[2], result->death_image[0]), auto_partitioner());
+        parallel_for(blocked_range<size_t>(0, size), merge_image(image[3], result->death_image[1]), auto_partitioner());
 
         return result;
     };
@@ -517,86 +527,96 @@ void process_replay_directory(const path& directory) {
                            tbb::make_filter<process_result*, void>(tbb::filter::parallel, f_clean_up));
 
 
-    auto get_bounds = [](const image_t<float> &image) -> std::tuple<float, float> {
+    auto get_bounds = [](const image_t<float> &image, float l_quant,float r_quant) -> std::tuple<float, float> {
         accumulator_t_right r(tag::tail<boost::accumulators::right>::cache_size = c);
         accumulator_t_left l(tag::tail<boost::accumulators::left>::cache_size = c);
         auto shape = image.shape();
         int size = static_cast<int>( shape[0]*shape[1] );
         for (int i = 0; i < size; ++i) {
+            if (*(image.data() + i) > 0.f) {
             r(*(image.data() + i));
             l(*(image.data() + i));
+            }
         }
-        auto lower_bound = quantile(l, quantile_probability = 0.25 );
-        auto upper_bound = quantile(r, quantile_probability = 0.75 );
+        auto lower_bound = quantile(l, quantile_probability = l_quant );
+        auto upper_bound = quantile(r, quantile_probability = r_quant );
         return std::make_tuple(lower_bound, upper_bound);
     };
 
-    typedef std::map<std::tuple<std::string, std::string>, std::array<image_t<float>, 2>>::value_type images_entry;
-    tbb::parallel_do(images.begin(), images.end(), [&](const images_entry &entry){
+    auto calculate_alpha = [](const std::tuple<float, float> &bounds, float value) -> float {
+        return
+        std::min(std::max((value - std::get<0>(bounds)) / (std::get<1>(bounds) - std::get<0>(bounds)), 0.f), 1.f);
+    };
+
+    auto mix = [](int v0, int v1, float a1, int v2, float a2) {
+        return (1-a2)*((1-a1)*v0 + a1*v1) + a2 * v2;
+    };
     
-        const std::array<image_t<float>,2> &image = entry.second;
-        std::string map_name, game_mode;
-        std::tie(map_name, game_mode) = entry.first;
-        auto shape = image[0].shape();
-        auto size = shape[0]*shape[1];
-        // float max = *std::max_element(image.data(), image.data() + size);
-        float avg[2] = {
-            std::accumulate(image[0].data(), image[0].data() + size, 0.f) / size,
-            std::accumulate(image[1].data(), image[1].data() + size, 0.f) / size
-        };
-        boost::multi_array<uint8_t, 3> base(boost::extents[500][500][4]);
-        auto base_shape = base.shape();
-        auto base_size = base_shape[0]*base_shape[1]*base_shape[2];
-        std::unique_ptr<png_bytep[]> row_pointers(new png_bytep[500]);
-        for (int i = 0; i < base_shape[0]; ++i) {
-            row_pointers[i] = base.origin() + i*base_shape[1]*base_shape[2];
-        }
-        int width, height, channels;
-        std::string mini_map = "maps/no-border/" + map_name + "_" + game_mode + ".png";
-        read_png2(mini_map, row_pointers.get(), width, height, channels);
-
+    auto draw_image = [&](boost::multi_array<uint8_t, 3> &base, const image_t<float> &team1, const image_t<float> &team2, float l_quant, float u_quant) {
         std::array<std::tuple<float, float>, 2> bounds = {
-            get_bounds(image[0]),
-            get_bounds(image[1])
+            get_bounds(team1, l_quant, u_quant),
+            get_bounds(team2, l_quant, u_quant)
         };
 
-        std::cout << std::get<0>(bounds[0]) << " "<< std::get<1>(bounds[0]) << "\n";
-        std::cout << std::get<0>(bounds[1]) << " "<< std::get<1>(bounds[1]) << "\n";
-        bool alpha = channels == 4;
-        for (int i = 0; i < 500; ++i) {
-            for (int j = 0; j < 500; ++j) {
-                    float a[2]  = {
-                        image[0][i][j]/(10*avg[0]),
-                        image[1][i][j]/(10*avg[1])
-                    };
-                
-                    a[0] = a[0] > 1.0 ? 1.0 : a[0];
-                    a[1] = a[1] > 1.0 ? 1.0 : a[1];
-                    // base[i][j][3] = a > 255 ? 0xFF : a;
-                    base[i][j][3] = 0xFF;
-                    //std::cout << image[i][j] / max << "\n";
-                    auto mix = [](int v0, int v1, float a1, int v2, float a2) {
-                        return (1-a2)*((1-a1)*v0 + a1*v1) + a2 * v2;
-                    };
-                
-                    base[i][j][0] = mix(base[i][j][0], 0, a[0], 255, a[1]);
-                    base[i][j][1] = mix(base[i][j][1], 255, a[0], 0, a[1]);
-                    base[i][j][2] = mix(base[i][j][2], 0, a[0], 0, a[1]);
+        auto shape = base.shape();
+        for (size_t i = 0; i < shape[0]; ++i) {
+            for (size_t j = 0; j < shape[1]; ++j) {                
+                float a[2]  = {
+                    calculate_alpha(bounds[0], team1[i][j]),
+                    calculate_alpha(bounds[1], team2[i][j])
+                };
+                base[i][j][0] = mix(base[i][j][0], 0, a[0], 255, a[1]);
+                base[i][j][1] = mix(base[i][j][1], 255, a[0], 0, a[1]);
+                base[i][j][2] = mix(base[i][j][2], 0, a[0], 0, a[1]);
             }
         }
-        std::stringstream out_file_name;
-        out_file_name << "out/" << map_name << "_" << game_mode << ".png";
+    };
 
-        int j = 0;
-        for (auto &i : image) {
-            std::stringstream ss;
-            ss << "out/values_" << map_name << "_" << game_mode << "_"
-                << j++ << ".txt";
-            ofstream os(ss.str(), ios::binary | ios::ate);
-            std::copy(i.data(), i.data() + size, std::ostream_iterator<float>(os, "\n"));
+    auto get_row_pointers = [](multi_array<uint8_t, 3> &image, std::vector<png_bytep> &row_pointers) {
+        auto shape = image.shape();
+        size_t rows = shape[0], column_bytes = shape[1]*shape[2];
+        for (int i = 0; i < rows; ++i) {
+            row_pointers.push_back(image.data() + i*column_bytes);
         }
-        write_png(out_file_name.str(), row_pointers.get(), width, height, alpha);
-                     });
+    };
+
+    auto read_mini_map = [&] (const std::string &map_name, const std::string &game_mode, boost::multi_array<uint8_t, 3> &base) {
+        base.resize(boost::extents[500][500][4]);
+        std::vector<png_bytep> row_pointers;
+        get_row_pointers(base, row_pointers);
+        int width, height, channels;
+        std::string mini_map = "maps/no-border/" + map_name + "_" + game_mode + ".png";
+        read_png2(mini_map, &row_pointers[0], width, height, channels);
+    };
+
+    auto write_image = [&](const std::string &file_name, boost::multi_array<uint8_t, 3> &image) {
+        auto shape = image.shape();
+        std::vector<png_bytep> row_pointers;
+        get_row_pointers(image, row_pointers);
+        write_png(file_name, &row_pointers[0], shape[0], shape[1], shape[2] == 4);
+    };
+
+    typedef std::map<std::tuple<std::string, std::string>, std::array<image_t<float>, 4>>::value_type images_entry;
+    tbb::parallel_do(images.begin(), images.end(), [&](const images_entry &entry){
+        const std::array<image_t<float>,4> &image = entry.second;
+        std::string map_name, game_mode;
+        std::tie(map_name, game_mode) = entry.first;
+        boost::multi_array<uint8_t, 3> base;
+
+        // process positions
+        read_mini_map(map_name, game_mode, base);
+        draw_image(base, image[0], image[1], 0.02, 0.98);
+        std::stringstream position_image;
+        position_image << "out/positions/" << map_name << "_" << game_mode << ".png";
+        write_image(position_image.str(), base);
+
+        // process deaths
+        read_mini_map(map_name, game_mode, base);
+        draw_image(base, image[2], image[3], 0.f, 1.f);
+        std::stringstream death_image;
+        death_image << "out/deaths/" << map_name << "_" << game_mode << ".png";
+        write_image(death_image.str(), base);
+    });
 }
 
 
@@ -607,8 +627,8 @@ auto get_bounds(const std::vector<float> &values) -> std::tuple<float, float> {
         r(value);
         l(value);
     }
-    auto lower_bound = quantile(l, quantile_probability = 0.25 );
-    auto upper_bound = quantile(r, quantile_probability = 0.75 );
+    auto lower_bound = quantile(l, quantile_probability = 0.05 );
+    auto upper_bound = quantile(r, quantile_probability = 0.95 );
     return std::make_tuple(lower_bound, upper_bound);
 };
 
@@ -623,9 +643,10 @@ int main(int argc, const char * argv[]) {
     std::cout << std::get<0>(bounds) << " " << std::get<1>(bounds) << "\n";
     // std::exit(1);
 
-    process_replay_directory("replays"); std::exit(1);
+     process_replay_directory("replays"); std::exit(1);
     
     string file_names[] = {
+         "replays/20120405_2204_ussr-KV_caucasus.wotreplay",
         "replays/20120707_2059_germany-E-75_himmelsdorf.wotreplay",
         "replays/20120815_0309_germany-E-75_02_malinovka.wotreplay",
         "replays/8.0/20120906_2352_germany-Panther_II_02_malinovka.wotreplay",
@@ -635,10 +656,11 @@ int main(int argc, const char * argv[]) {
         "replays/20120826_2059_france-AMX_13_90_02_malinovka.wotreplay",
         "replays/20120826_1729_france-AMX_13_90_04_himmelsdorf.wotreplay",
         "replays/20120920_2130_france-AMX_13_90_14_siegfried_line.wotreplay",
-        "replays/20120921_0042_ussr-IS-3_02_malinovka.wotreplay"
+        "replays/20120921_0042_ussr-IS-3_02_malinovka.wotreplay",
+       
     };
 
-    auto file_name = file_names[8];
+    auto file_name = file_names[2];
     ifstream is(file_name, std::ios::binary);
 
     if (!is) {
