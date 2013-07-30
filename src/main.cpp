da5129f61ddc62d6bc5325f0b2f9d8fb886673d6
Jan Temmerman <jantemmerman@gmail.com>
2013-07-30 21:08:42 +0200

    remove experimental code

--- src/main.cpp
+++ src/main.cpp
@@ -115,35 +115,6 @@ int main(int argc, const char * argv[]) {
     // find values
     const auto &packets = game.get_packets();
     show_packet_summary(packets);
-    std::set<uint32_t> ids;
-    for (auto it = packets.begin(); it != packets.end(); ++it) {
-        if (it->has_property(property_t::player_id)) {
-            if (ids.insert(it->player_id()).second && it->clock() > 10.f) {
-                std::cout << it->player_id() << "\n";
-            }
-        }  
-            auto &data = it->get_data();
-            if (it->has_property(property_t::position)) {
-                auto position = it->position();
-                
-                auto x = get_field<float>(data.begin(), data.end(), 41);
-                auto y = get_field<float>(data.begin(), data.end(), 45);
-                auto z = get_field<float>(data.begin(), data.end(), 49);
-                auto m = get_field<float>(data.begin(), data.end(), 53);
-//                std::cout << boost::format("[%1%] clock=%2% player_id=%3%") % std::distance(packets.begin(), it) % it->clock() % it->player_id();
-//                std::cout << boost::format(" position=(%1%,%2%,%3%) hull_orientation=(%5%,%6%,%7%)") % std::get<0>(position) % std::get<1>(position) % std::get<2>(position) % x % y % z % m;
-//                std::cout << std::endl;
-//                display_packet(*it);
-            } else if (it->type() == 0x07 && get_field<unsigned char>(data.begin(), data.end(), 13) == 0x03) {
-
-                std::cout << boost::format("match: %1%\n") % get_field<unsigned short>(data.begin(), data.end(), 21);
-                display_packet(*it);
-            } else {
-                // display_packet(*it);
-            } 
-    }
-
-    std::cout << boost::format("Found %1% player-ids\n") % ids.size();
 
     return EXIT_SUCCESS;
 }
